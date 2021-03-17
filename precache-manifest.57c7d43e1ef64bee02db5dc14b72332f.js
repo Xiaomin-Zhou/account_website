31114bc58e545bc9fe557cf0f323d132e03574b9
Zhou Xiaomin <2249631832@qq.com>
2021-03-17 22:07:33 +0800

    deploy

---
+++ precache-manifest.57c7d43e1ef64bee02db5dc14b72332f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6cf5ac2d9f26e574e924ecf492cca9",
+    "url": "/account_website/Qr_code.png"
+  },
+  {
+    "revision": "e0fc043f8e1b41c3b822",
+    "url": "/account_website/css/app.9a9a9dc7.css"
+  },
+  {
+    "revision": "5ae7d12493f09df613ef469401fbaa4d",
+    "url": "/account_website/index.html"
+  },
+  {
+    "revision": "e0fc043f8e1b41c3b822",
+    "url": "/account_website/js/app.679acc84.js"
+  },
+  {
+    "revision": "8465474607561b25b243",
+    "url": "/account_website/js/chunk-vendors.e2952a05.js"
+  },
+  {
+    "revision": "1e0e1af34bbc4494c22f6dad8591c23b",
+    "url": "/account_website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account_website/robots.txt"
+  }
+]);
